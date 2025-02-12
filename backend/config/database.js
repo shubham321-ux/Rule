bb0e5f2a6b3e661c52f29d98616cf24ebaf37c8c
shubham <141753431+shubham321-ux@users.noreply.github.com>
2025-02-12 11:22:02 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 export const connectMongo = async () => {
     try {
         await mongoose.connect(process.env.DB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
         });
         console.log("Database connected successfully");
     } catch (err) {
