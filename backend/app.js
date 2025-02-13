63f270c74e99bbe8b75f994e21d6e825271e9280
shubham321-ux <shubham4260shubhu@gmail.com>
2025-02-13 12:22:38 +0530

    token sended from local storage

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ const app = express();
 
 // CORS Configuration
 app.use(cors({
-    origin: ['https://bokifa.netlify.app','http://localhost:3002', process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
